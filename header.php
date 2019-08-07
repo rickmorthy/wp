fce22febc2a265d19c465d5f761ed76843c7039e
iceforge@porotonmai.com <iceforge@porotonmai.com>
2019-08-07 16:12:12 -0400

    index footer header style

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/style.css">
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
